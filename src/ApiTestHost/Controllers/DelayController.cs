34ebf98c7697e80456990d12ff565dbcd38daa38
Marc Schmidt <masch212@outlook.de>
2021-03-26 20:25:45 +0100

    Added project with json-server and deelay functionalities

---
+++ src/ApiTestHost/Controllers/DelayController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ApiTestHost.Controllers
+{
+    [Route("delay")]
+    public class DelayController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<DelayController>? _logger;
+
+        public DelayController(ILogger<DelayController>? logger)
+        {
+            _httpClient = new HttpClient();
+            _logger = logger;
+        }
+
+        [Route("{delay}/{*url}")]
+        public async Task Index(int delay, string url)
+        {
+            _logger.LogInformation("Delaying request to \"{0}\" by {1} milliseconds...", url, delay);
+            await Task.Delay(delay);
+
+            var targetUri = new UriBuilder(url) { Query = HttpContext.Request.QueryString.Value }.Uri;
+
+            var req = HttpContext.Request;
+            var request = new HttpRequestMessage(new HttpMethod(req.Method), targetUri);
+            foreach (var header in req.Headers)
+                request.Headers.TryAddWithoutValidation(header.Key, header.Value.AsEnumerable());
+            request.Content = new StreamContent(req.Body);
+            if (req.Headers.ContainsKey("Content-Type"))
+                request.Content.Headers.Add("Content-Type", req.ContentType);
+
+            _logger.LogInformation("Sending request to \"{0}\"...", url);
+            var response = await _httpClient.SendAsync(request);
+            _logger.LogInformation("Got response from \"{0}\". Redirecting response...", url);
+
+            var resp = HttpContext.Response;
+            resp.StatusCode = (int)response.StatusCode;
+            foreach (var header in response.Headers)
+                resp.Headers[header.Key] = new StringValues(header.Value.ToArray());
+            if (response.Content.Headers.Contains("Content-Type"))
+                resp.ContentType = response.Content.Headers.GetValues("Content-Type").Single();
+            using (var stream = await response.Content.ReadAsStreamAsync())
+            using (var bodyStream = resp.BodyWriter.AsStream())
+            {
+                await stream.CopyToAsync(bodyStream);
+            }
+        }
+    }
+}
---
+++ src/ApiTestHost/Controllers/JsonController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ApiTestHost.Controllers
+{
+    [Route("json")]
+    public class JsonController : Controller
+    {
+        private readonly ILogger<JsonController>? _logger;
+
+        public JsonController(ILogger<JsonController>? logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{*url}")]
+        public async Task<IActionResult> GetJson(string url)
+        {
+            if (!TryGetFilePath(url, out var filePath))
+                return NotFound();
+
+            _logger.LogInformation("Sending requested file: {0}", filePath);
+            var json = await System.IO.File.ReadAllTextAsync(filePath);
+            return Content(json, "application/json");
+        }
+
+        [HttpDelete("{*url}")]
+        public IActionResult DeleteJson(string url)
+        {
+            if (!TryGetFilePath(url, out var filePath))
+                return NotFound();
+
+            _logger.LogInformation("Deleting requested file: {0}", filePath);
+            System.IO.File.Delete(filePath);
+            return Ok();
+        }
+
+        [HttpPatch("{*url}")]
+        [HttpPost("{*url}")]
+        [HttpPut("{*url}")]
+        public async Task<IActionResult> WriteJson(string url)
+        {
+            TryGetFilePath(url, out var filePath, false);
+
+            string json;
+            using (var reader = new StreamReader(HttpContext.Request.Body))
+                json = await reader.ReadToEndAsync();
+
+            _logger.LogInformation("Writing requested file: {0}", filePath);
+            await System.IO.File.WriteAllTextAsync(filePath, json);
+            return Ok();
+        }
+
+        private bool TryGetFilePath(string url, out string filePath, bool logNotFound = true)
+        {
+            filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "json", url + ".json");
+            if (!System.IO.File.Exists(filePath))
+            {
+                if (logNotFound)
+                    _logger.LogError("Could not find requested file: {0}", filePath);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
